699bbc8694b2cd4251b7b1e040b80eedff71a355
Recito Gigih P <recitogigih9@gmail.com>
2020-01-13 16:22:30 +0700

    encrypt password

--- server/models/user.js
+++ server/models/user.js
@@ -1,46 +1,63 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt') //pasword hashing
+const SALT_I = 10;
 
 const userSchema = mongoose.Schema({
 
-    email:{
-        type:String,
-        required:true,
-        trim:true,
-        unique:1
+    email: {
+        type: String,
+        required: true,
+        trim: true,
+        unique: 1
     },
-    password:{
-        type:String,
-        required:true,
-        minlength:5
+    password: {
+        type: String,
+        required: true,
+        minlength: 5
     },
-    name:{
-        type:String,
-        required:true,
-        maxlength:100
+    name: {
+        type: String,
+        required: true,
+        maxlength: 100
     },
-    lastname:{
-        type:String,
-        required:true,
-        maxlength:100
+    lastname: {
+        type: String,
+        required: true,
+        maxlength: 100
     },
-    cart:{
-        type:Array,
-        default:[]
+    cart: {
+        type: Array,
+        default: []
     },
-    history:{
-        type:Array,
-        default:[]  
+    history: {
+        type: Array,
+        default: []
     },
-    role:{
-        type:Number,
-        default:0
+    role: {
+        type: Number,
+        default: 0
     },
-    token:{
-        type:String
+    token: {
+        type: String
     }
-
 })
 
-const User = mongoose.model('User',userSchema);
+userSchema.pre('save', function (next) { //before do anything, do this.
+    var user = this;
 
-module.exports = {User}
+    bcrypt.genSalt(SALT_I, function (err, salt) {
+        if (err) return next(err);
+
+        bcrypt.hash(user.password, salt, function (err, hash) {
+            if (err) return next(err);
+            user.password = hash;
+            next();
+        })
+    })
+})
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = {
+    User
+}
